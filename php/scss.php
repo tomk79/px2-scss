63d7ce9b81370349bf3f3ef2fe7b90ab2fe14b44
Tomoya Koyanagi <tomk79@gmail.com>
2020-04-01 09:40:50 +0900

    SCSS変換結果をキャッシュするようになった。

--- php/scss.php
+++ php/scss.php
@@ -12,14 +12,34 @@ class scss{
 	 * @param object $json プラグイン設定
 	 */
 	public static function processor( $px, $json ){
+		$realpath_cache = $px->get_realpath_homedir();
+		$realpath_cache .= '_sys/ram/caches/px2scss/';
+		if( !is_dir($realpath_cache) ){
+			$px->fs()->mkdir($realpath_cache);
+		}
+
 		foreach( $px->bowl()->get_keys() as $key ){
 			$src = $px->bowl()->pull( $key );
+
+			// キャッシュチェック
+			$src_md5 = md5($src);
+			clearstatcache();
+			if( $px->fs()->is_file($realpath_cache.$src_md5) ){
+				// キャッシュがあったらそのまま返す
+				$src = file_get_contents($realpath_cache.$src_md5);
+				$px->bowl()->replace( $src, $key );
+				continue;
+			}
+
 
 			$tmp_current_dir = realpath('./');
 			chdir( dirname( $_SERVER['SCRIPT_FILENAME'] ) );
 			$scss = new \Leafo\ScssPhp\Compiler();
 			$src = $scss->compile( $src );
 			chdir( $tmp_current_dir );
+
+			// キャッシュする
+			file_put_contents($realpath_cache.$src_md5, $src);
 
 			$px->bowl()->replace( $src, $key );
 		}
